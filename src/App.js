0cab98befbad400bdf66b1f7bcd17f3b5658b07c
ClaraPrimeJohn <primiel1423@gmail.com>
2025-02-21 20:17:19 +0800

    removed scroll effect

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import { useState, useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer"; 
@@ -12,36 +11,23 @@ import Login from "./components/Login";
 import Profile from "./pages/Profile"; 
 import Signup from "./components/Signup";
 import ForgotPassword from "./components/ForgotPassword";
-import ScrollToTop from "./components/ScrollToTop";
 
 function App() {
-    const [isScrolled, setIsScrolled] = useState(false);
-
-    useEffect(() => {
-        const handleScroll = () => {
-            setIsScrolled(window.scrollY > 10);
-        };
-
-        window.addEventListener("scroll", handleScroll);
-        return () => window.removeEventListener("scroll", handleScroll);
-    }, []);
-
     return (
         <Router>
-            <ScrollToTop />
-            <AppContent isScrolled={isScrolled} />
+            <AppContent />
         </Router>
     );
 }
 
-function AppContent({ isScrolled }) {
+function AppContent() {
     const location = useLocation();
     const hideNavbarRoutes = ["/login", "/signup", "/forgot"]; 
 
     return (
         <div className="flex flex-col min-h-screen">
-            {!hideNavbarRoutes.includes(location.pathname) && <Navbar isScrolled={isScrolled} />}
-            <main className={`flex-grow transition-all duration-1000 ${isScrolled ? "pt-16" : "pt-0"}`}>
+            {!hideNavbarRoutes.includes(location.pathname) && <Navbar />}
+            <main className="flex-grow">
                 <Routes>
                     <Route path="/" element={<Home />} />
                     <Route path="/about-us" element={<AboutUs />} />
