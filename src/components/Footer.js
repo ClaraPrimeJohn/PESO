fb48031cbb543a79f50f94b9d496bfc9bc732624
ClaraPrimeJohn <primiel1423@gmail.com>
2025-02-25 13:46:05 +0800

    oca less - navbar from orange to gray

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,9 @@
 const Footer = () => {
   return (
-    <footer className="bg-orange py-5 mt-auto"> 
+    <footer className="bg-gray-300 py-5 mt-auto"> 
     {/* PAA */}
       <div className="mx-auto px-6 lg:px-12">
-        <div className="text-center text-white text-sm">
+        <div className="text-center text-black   text-sm">
           Copyright © 2024 All Rights Reserved by The Researchers
         </div>
       </div>
