bec934a80b82ee757bc4f7ab0cab0b8c01847cab
ClaraPrimeJohn <primiel1423@gmail.com>
2025-03-27 11:19:19 +0800

    added lazy loading removed framer motion

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,7 +16,16 @@ module.exports = {
         'black-primary': "#525252",
         'black-secondary': "#404040",
         'gray-secondary': "#737373",
-      }, 
+      },
+      keyframes: {
+        float: {
+          '0%, 100%': { transform: 'translateY(0)' },
+          '50%': { transform: 'translateY(-5px)' },
+        }
+      },
+      animation: {
+        float: 'float 1.8s ease-in-out infinite',
+      }
     },
   },
   plugins: [],
