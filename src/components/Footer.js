d05a181ece9de6043f206eeabace837fee131914
ClaraPrimeJohn <primiel1423@gmail.com>
2025-05-16 19:11:09 +0800

    tanggal footer tamad mode

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,9 @@
 const Footer = () => {
   return (
-    <footer className="bg-gray-300 py-5 mt-auto"> 
+    <footer className=""> 
     {/* PAA */}
-      <div className="mx-auto px-6 lg:px-12">
-        <div className="text-center text-black text-sm">
-          Copyright © 2024 All Rights Reserved by The Researchers
+      <div className="">
+        <div className="">
         </div>
       </div>
     </footer>
